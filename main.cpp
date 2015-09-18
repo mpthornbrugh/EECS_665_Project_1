028f61b549c3b566f6061eb66cd029e2870288c0
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-09-18 00:23:55 -0500

    Modified main

--- main.cpp
+++ main.cpp
@@ -7,16 +7,50 @@
 //
 
 #include <iostream>
+#include <string>
+
+std::string removeBraces(std::string s) {
+	s.erase(0, 1);
+	s.erase(s.size() - 1);
+	return s;
+}
 
 int main(int argc, const char * argv[])
-{
-    if (argc < 1) {
-        std::cout << "The file requires an input filename." << std::endl;
-        return 1;
+{   
+    std::string x;
+
+    for (int i = 0; i < 2; i++) {//Removing 'Initial State'
+    	std::cin >> x;
     }
-    
-    
-    
+
+    std::cin >> x;
+
+    x = removeBraces(x);
+
+    int initialState = std::atoi(x.c_str());
+
+    for (int i = 0; i < 2; i++) {//Removing 'Final State'
+    	std::cin >> x;
+    }
+
+    std::cin >> x;
+
+    x = removeBraces(x);
+
+    int finalState = std::atoi(x.c_str());
+
+    for (int i = 0; i < 2; i++) {//Removing 'Total States'
+    	std::cin >> x;
+    }
+
+    std::cin >> x;
+
+    int numStates = std::atoi(x.c_str());
+
+    std::cout << "Start State: " << initialState << std::endl;
+    std::cout << "Final State: " << finalState << std::endl;
+    std::cout << "Number of States: " << numStates << std::endl;
+
     return 0;
 }
 
