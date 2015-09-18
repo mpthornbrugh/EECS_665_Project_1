39899a872d19c48f199c758d50fad655a5fbfe18
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-09-17 23:30:47 -0500

    Initial Files

---
+++ Project_1/Project_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project_1
+//
+//  Created by Michael Thornbrugh on 9/17/15.
+//  Copyright (c) 2015 Michael Thornbrugh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Project_1
+//
+//  Created by Michael Thornbrugh on 9/17/15.
+//  Copyright (c) 2015 Michael Thornbrugh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    if (argc < 1) {
+        std::cout << "The file requires an input filename." << std::endl;
+        return 1;
+    }
+    
+    
+    
+    return 0;
+}
+
