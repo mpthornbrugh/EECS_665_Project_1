847e84057217cafb0bf0e2284f1d35bce90d0418
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-09-23 12:50:34 -0500

    Changes

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ std::string findEClosure(Node** statesArray, int state, int numStates) {
 
     for (int i = 0; i < numStates; i++) {
         if (visitedArr[i]) {
-            closure += string(itoa(i+1));
+            closure += std::to_string(i+1);
             closure += ",";
         }
     }
